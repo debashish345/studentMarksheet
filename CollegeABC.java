c832cfa684692b816a7901af3b6fc6c7a796e961
debashish345 <31614783+debashish345@users.noreply.github.com>
2019-09-14 21:23:01 +0530

    Update CollegeABC.java

--- CollegeABC.java
+++ CollegeABC.java
@@ -8,32 +8,36 @@ int s_no;
 String[] s_names= new String[s_no];
 int s_marks[]= new int[s_no];  
 
+//setter method
 
 void setMarksheet(){
 
-System.out.println(s_no);
 System.out.println("Enter student name and marks: ");
 String[] dup_names= new String[s_no];
 int dup_marks[]= new int[s_no];
 
+s_names = dup_names;
+s_marks = dup_marks;
+
 for(int i=0;i<s_no;i++){
-System.out.printf("enter %d student name:\t",i);
+System.out.printf("enter %d student name:\t",i+1);
 dup_names[i] = n.nextLine();
 
 System.out.printf("enter %s marks:\t",s_names[i]);
 dup_marks[i] = n.nextInt();
 n.nextLine();
 }
-s_names = dup_names;
-s_marks = dup_marks;
 }
 
+//getter method
+
 void getMarksheet(){
-System.out.printf("*****marksheet*****\n\n");
+System.out.printf("\n\n*****marksheet*****\n\n");
 for(int i = 0;i<s_no;i++){
 System.out.printf("%s get %d\n\n",s_names[i],s_marks[i]);
 }
 }
+
 }
 
 
@@ -48,5 +52,6 @@ class CollegeABC
 	class_nine.s_no = num;
 	
 	class_nine.setMarksheet();
+	class_nine.getMarksheet();
 	}
 }
